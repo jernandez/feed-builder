5b8640363693b94fd9787c13c631c1c27bd8627a
Ben Balentine <ben.balentine@bbalentine-mbpro.(none)>
2012-09-18 09:28:47 -0500

    updated feed to pull data from mongodb instead of a specified file

--- feed_generator.py
+++ feed_generator.py
@@ -1,16 +1,19 @@
 #! /usr/bin/env python
-import time,sys, getopt, re
+import time,sys, getopt
 from xml.etree.ElementTree import *
 from xml.dom.minidom import parseString
+import pymongo
 
-###################################################################
-# to dos
-###################################################################
-# product families
-# multi locales
-# error checking
-# accept excel files for inputs
+from pymongo import Connection
 
+
+'''
+	product_id = #apihostname.find({}, {_id:1,client:1})  id is the apihostname, client is the externalid
+	productName = #sites.find({}, {_client._name:1})
+	productDesc = #sites.find({}, {_client._name:1})
+	productUrl = #apihostname.find({}, {_id:1,client:1,_clientDomainNames:1})  id is the apihostname, client is the externalid
+	imageUrl = #concatenate apihostname+static+_customerLogoImageName sites.find({}, {_client._name:1,_customerLogoImageName:1})
+'''
 
 ###################################################################
 # handle command line args
@@ -19,7 +22,7 @@ from xml.dom.minidom import parseString
 def main(argv):                         
               
     try:                                
-        opts, args = getopt.getopt(argv, "hc:i:o:s:", ["help", "clientname=", "infile=", "outfile=", "schema="]) 
+        opts, args = getopt.getopt(argv, "h:o:s:", ["help", "outfile=", "schema="]) 
     except getopt.GetoptError:           
         usage()                          
         sys.exit(2)       
@@ -27,12 +30,6 @@ def main(argv):
     	if opt in ("-h", "--help"):
     		usage()
     		sys.exit(2)
-    	elif opt in ("-c", "--clientName"):
-    		global clientName
-    		clientName = arg
-    	elif opt in ("-i", "--infile"):
-    		global infile
-    		infile = arg
     	elif opt in ("-o", "--outfile"):
     		global outfile
     		outfile = arg
@@ -48,8 +45,11 @@ if __name__ == "__main__":
 
 
 
-#clientName = 'bridgestone'
-clientFile = open(infile)
+#open connection to mongodb
+connection=Connection('qaclientconfigstore.lab', 27017)
+db=connection.configs
+
+
 clientProductFeed = open(outfile, 'w')
 generateDateTime = time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime())
 schemaVersion = 'http://www.bazaarvoice.com/xs/PRR/ProductFeed/' + schema
@@ -58,64 +58,32 @@ incrementalValue = 'false'
 #build necessary header
 root = Element('Feed')
 root.set('incremental', incrementalValue)
-root.set('name', clientName)
+root.set('name', 'bazaarvoice')
 root.set('xmlns', schemaVersion)
 root.set('extractDate', generateDateTime)
 
 # 
-brands = SubElement(root, 'Brands')
-categories = SubElement(root, 'Categories')
 products = SubElement(root, 'Products')
 
-#
+#query database for client name, logo name, and domain name
+clients=list(db.sites.find({}, {'_displayCodeKey':1,'_client._name':1,'_clientDomainNames':1}))
+for row in clients:
+	product_id = row['_client']['_name']
+	productName = row['_client']['_name']
+	productDesc = row['_client']['_name']
+	productUrl = 'http://'+row['_clientDomainNames'][0]
+	imageUrl = 'http://'+row['_client']['_name']+'.ugc.bazaarvoice.com/static/'+row['_displayCodeKey']+'/logo.gif'
+	
+	product = SubElement(products, 'Product')
+	externalIDNode = SubElement(product, 'ExternalId')
+	externalIDNode.text = product_id
+	nameNode = SubElement(product, 'Name')
+	nameNode.text = productName
+	descNode = SubElement(product, 'Description')
+	descNode.text = productDesc
+	pdpURLNode = SubElement(product, 'ProductPageUrl')
+	pdpURLNode.text = productUrl
+	imageNode = SubElement(product, 'ImageUrl')
+	imageNode.text = imageUrl
 
-for line in clientFile:
-	vals = line.split('	')
-	externalId = vals[0]
-	recordType = vals[1]
-	recordName = vals[2]
-	productDesc = vals[3]
-	productUrl = vals[4]
-	imageUrl = vals[5]
-	categoryId = vals[6]
-
-	# pipe delimited values
-	manufactureNums = vals[7]
-	brand = vals[8]
-	locale  = vals[9]
-	#ISBN = vals[10]
-	#EAN = vals[11]
-	#UPC = vals[12]
-
-
-	if recordType == 'Product':
-		product = SubElement(products, 'Product')
-		externalIDNode = SubElement(product, 'ExternalId')
-		externalIDNode.text = externalId
-		nameNode = SubElement(product, 'Name')
-		nameNode.text = recordName
-		descNode = SubElement(product, 'Description')
-		descNode.text = productDesc
-		pdpURLNode = SubElement(product, 'ProductPageUrl')
-		pdpURLNode.text = productUrl
-		imageNode = SubElement(product, 'ImageUrl')
-		imageNode.text = imageUrl
-	elif recordType == 'category':
-		category = SubElement(categories, 'Category')
-		externalIDNode = SubElement(category, 'ExternalId')
-		externalIDNode.text = externalId
-		nameNode = SubElement(category, 'Name')
-		nameNode.text = recordName
-		pdpURLNode = SubElement(category, 'CategoryPageUrl')
-		pdpURLNode.text = productUrl
-		if categoryId != externalId:
-			parentCatNode = SubElement(category, 'ParentExternalId')
-			parentCatNode.text = categoryId
-	#elif recordType == 'brand':
-	#	brand = SubElement(brands, 'Brand')
-	#	externalIDNode = SubElement(brand, 'ExternalId')
-	#	externalIDNode.text = externalId
-	#	nameNode = SubElement(brand, 'Name')
-	#	nameNode.text = recordName
-		
 clientProductFeed.write(tostring(root))
