f386d8f27d04af23bf8770f272d18bdca329c65e
javier.hernandez <javier.hernandez@bazaarvoice.com>
2012-09-23 21:16:28 -0500

    rearchitected by Kevin Nayar

--- feed_generator.py
+++ feed_generator.py
@@ -1,124 +1,153 @@
 #! /usr/bin/env python
-import time,sys, getopt, re
+import time, sys, csv
 from xml.etree.ElementTree import *
 from xml.dom.minidom import parseString
+from optparse import OptionParser
 
 ###################################################################
-# to dos
+# Nuts and bolts
 ###################################################################
-# product families
-# multi locales
-# error checking
-# accept excel files for inputs
+def populateTags(parentTag, tagTitle, tagText):
+	node = SubElement(parentTag, tagTitle)
+	node.text = tagText
 
+def checkForExistence(line, num, errorMsg):
+	try: 
+		line[num]
+		return True
+	except IndexError:
+		print errorMsg
 
-###################################################################
-# handle command line args
-###################################################################
+def generateFeed(options):
+	# Access files
+	clientFile = open(options.input)
+	clientProductFeed = open(options.output, 'w')
+	dialect = csv.Sniffer().sniff(clientFile.read(1024))
+	clientFile.seek(0)
+	reader = csv.reader(clientFile, dialect)
 
-def main(argv):                         
-              
-    try:                                
-        opts, args = getopt.getopt(argv, "hc:i:o:s:v:", ["help", "clientname=", "infile=", "outfile=", "schema=", "incrementalValue="]) 
-    except getopt.GetoptError:           
-        usage()                          
-        sys.exit(2)       
-    for opt, arg in opts: 
-    	if opt in ("-h", "--help"):
-    		usage()
-    		sys.exit(2)
-    	elif opt in ("-c", "--clientName"):
-    		global clientName
-    		clientName = arg
-    	elif opt in ("-i", "--infile"):
-    		global infile
-    		infile = arg 
-    	elif opt in ("-o", "--outfile"):
-    		global outfile
-    		outfile = arg 
-    	elif opt in ("-s", "--schema"):
-    		global schema
-    		schema = arg
-    	elif opt in ("-v", "--incrementalValue"):
-    		global incrementalValue
-    		incrementalValue = arg
+	# Define Feed tag values
+	generateDateTime = time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime())
+	schemaVersion = 'http://www.bazaarvoice.com/xs/PRR/ProductFeed/' + options.schema
+	incrementalValue = 'false'
 
-    	
-if __name__ == "__main__":
-    main(sys.argv[1:])		
+	# Build necessary header
+	xmlPrefix = '<?xml version="1.0" encoding="UTF-8"?>'
+	root = Element('Feed')
+	root.set('incremental', incrementalValue)
+	root.set('name', options.clientName)
+	root.set('xmlns', schemaVersion)
+	root.set('extractDate', generateDateTime)
 
-###################################################################
-###################################################################
+	# Define top-level elements
+	brands = SubElement(root, 'Brands')
+	categories = SubElement(root, 'Categories')
+	products = SubElement(root, 'Products')
 
+	# Loop through input
+	reader.next() # This starts the iteration at row two, thereby ignoring the first row with titles
+	for line in reader:
+		productName = line[0]
+		productId = line[1]
+		productDesc = line[2]
+		productPageUrl = line[3]
+		productImageUrl = line[4]
+		categoryName = line[5]
+		categoryId = line[6]
+		brandName = line[7]
+		brandId = line[8]
 
-
-#clientName = 'bridgestone'
-clientFile = open(infile)
-clientProductFeed = open(outfile, 'w')
-generateDateTime = time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime())
-schemaVersion = 'http://www.bazaarvoice.com/xs/PRR/ProductFeed/' + schema
-
-#build necessary header
-root = Element('Feed')
-root.set('incremental', incrementalValue)
-root.set('name', clientName)
-root.set('xmlns', schemaVersion)
-root.set('extractDate', generateDateTime)
-
-# 
-brands = SubElement(root, 'Brands')
-categories = SubElement(root, 'Categories')
-products = SubElement(root, 'Products')
-
-#
-
-for line in clientFile:
-	vals = line.split('	')
-	externalId = vals[0]
-	recordType = vals[1]
-	recordName = vals[2]
-	productDesc = vals[3]
-	productUrl = vals[4]
-	imageUrl = vals[5]
-	categoryId = vals[6]
-
-	# pipe delimited values
-	manufactureNums = vals[7]
-	brand = vals[8]
-	locale  = vals[9]
-	ISBN = vals[10]
-	EAN = vals[11]
-	UPC = vals[12]
-
-
-	if recordType == 'Product':
 		product = SubElement(products, 'Product')
-		externalIDNode = SubElement(product, 'ExternalId')
-		externalIDNode.text = externalId
-		nameNode = SubElement(product, 'Name')
-		nameNode.text = recordName
-		descNode = SubElement(product, 'Description')
-		descNode.text = productDesc
-		pdpURLNode = SubElement(product, 'ProductPageUrl')
-		pdpURLNode.text = productUrl
-		imageNode = SubElement(product, 'ImageUrl')
-		imageNode.text = imageUrl
-	elif recordType == 'category':
 		category = SubElement(categories, 'Category')
-		externalIDNode = SubElement(category, 'ExternalId')
-		externalIDNode.text = externalId
-		nameNode = SubElement(category, 'Name')
-		nameNode.text = recordName
-		pdpURLNode = SubElement(category, 'CategoryPageUrl')
-		pdpURLNode.text = productUrl
-		if categoryId != externalId:
-			parentCatNode = SubElement(category, 'ParentExternalId')
-			parentCatNode.text = categoryId
-	elif recordType == 'brand':
 		brand = SubElement(brands, 'Brand')
-		externalIDNode = SubElement(brand, 'ExternalId')
-		externalIDNode.text = externalId
-		nameNode = SubElement(brand, 'Name')
-		nameNode.text = recordName
+
+		# Secondary product information (mfg. part no, UPC, model no., etc.)
+		if checkForExistence(line, 9, 'Secondary product information is missing (Mfg. Part Number)'):
+			mfgPartNumbers = SubElement(product, 'ManufacturerPartNumbers')
+			populateTags(mfgPartNumbers, 'ManufacturerPartNumber', line[9])
+		if checkForExistence(line, 10, 'Secondary product information is missing (UPC)'):
+			upcs = SubElement(product, 'UPCs')
+			populateTags(upcs, 'UPC', line[10])
+		if checkForExistence(line, 11, 'Secondary product information is missing (Model Number)'):
+			modelNumbers = SubElement(product, 'ModelNumbers')
+			populateTags(modelNumbers, 'ModelNumber', line[11])
+
+		# Product families
+		# if checkForExistence(line, 12, 'Product families information is missing'):
+		if line[12] != '':
+			attrs = SubElement(product, 'Attributes')
+			familyAttr = SubElement(attrs, 'Attribute')
+			familyAttr.set('id', 'BV_FE_FAMILY')
+			familyValue = SubElement(familyAttr, 'Value')
+			familyValue.text = line[12]
+			if line[13] != '':
+				expandAttr = SubElement(attrs, 'Attribute')
+				expandAttr.set('id', 'BV_FE_EXPAND')
+				expandValue = SubElement(expandAttr, 'Value')
+				expandValue.text = 'BV_FE_FAMILY:' + line[12]
+
+		# Define lists
+		productList = {
+			'Name': productName, 
+			'ExternalId': productId,
+			'Description': productDesc,
+			'ProductPageUrl': productPageUrl,
+			'ImageUrl': productImageUrl,
+			'CategoryExternalId': categoryId,
+			'BrandExternalId': brandId
+		}
+
+		categoryList = {
+			'Name': categoryName, 
+			'ExternalId': categoryId
+		}
+
+		brandList = {
+			'Name': brandName, 
+			'ExternalId': brandId
+		}
+
+		# for key, value in productList.items():
+		# 	populateTags(product, key, value)
+
+		# for key, value in categoryList.items():
+		# 	populateTags(category, key, value)
 		
-clientProductFeed.write(parseString(tostring(root)).toprettyxml())
+		# for key, value in brandList.items():
+		# 	populateTags(brand, key, value)
+
+		######## Ignore the 3 statements above and just do it all at once instead (below).  
+		######## Unsure which method is more efficient/faster.  The second is probably 
+		######## more scalable and elegant. So, I'll roll with that shizz for now.
+
+		elementsMapToLists = {
+			product: productList,
+			category: categoryList,
+			brand: brandList
+		}
+
+		for key, value in elementsMapToLists.items():
+			for k, v in value.items():
+				populateTags(key, k, v)
+
+	clientProductFeed.write(xmlPrefix)
+	clientProductFeed.write(tostring(root))
+
+###################################################################
+# Handle command line args
+###################################################################
+
+def main(argv):
+	usage = 'usage: %prog [options] arg'
+	parser = OptionParser(usage)
+	parser.add_option('-c', '--clientName', help='Database name for the client', action='store', dest='clientName')
+	parser.add_option('-i', '--input', help='Location of the CSV input file', action='store', dest='input')
+	parser.add_option('-o', '--output', help='Location of the XML output file', action='store', dest='output')
+	parser.add_option('-s', '--schema', default='5.1', help='The Bazaarvoice XML schema version', action='store', dest='schema')
+	
+	(options, args) = parser.parse_args()
+
+	generateFeed(options)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])	
