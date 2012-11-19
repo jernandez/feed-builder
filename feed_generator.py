89d1e0109bad1b5904116fd7898794e9cd6a63a1
jwkozel <jwkozel@gmail.com>
2012-11-19 17:14:57 -0600

    several changes--adjusted for unique values, fixed brands

--- feed_generator.py
+++ feed_generator.py
@@ -13,8 +13,10 @@ def populateTags(parentTag, tagTitle, tagText):
 
 def checkForExistence(line, num, errorMsg):
 	try: 
-		line[num]
-		return True
+		if line[num] == '':
+			print errorMsg
+			return False
+		return True			
 	except IndexError:
 		print errorMsg
 
@@ -22,7 +24,7 @@ def generateFeed(options):
 	# Access files
 	clientFile = open(options.input)
 	clientProductFeed = open(options.output, 'w')
-	dialect = csv.Sniffer().sniff(clientFile.read(1024))
+	dialect = csv.Sniffer().sniff(clientFile.read())
 	clientFile.seek(0)
 	reader = csv.reader(clientFile, dialect)
 
@@ -46,6 +48,12 @@ def generateFeed(options):
 
 	# Loop through input
 	reader.next() # This starts the iteration at row two, thereby, ignoring the first row with titles
+
+	# utility
+	product_ids = {}
+	brand_dict = {}
+	category_dict = {}
+
 	for line in reader:
 		productName = line[0]
 		productId = line[1]
@@ -54,40 +62,53 @@ def generateFeed(options):
 		productImageUrl = line[4]
 		categoryName = line[5]
 		categoryId = line[6]
-		brandName = line[7]
-		brandId = line[8]
+
+		if product_ids.get(productId, False):
+			continue
+
+		product_ids[productId] = True
 
 		# Define individual top-level elements
 		product = SubElement(products, 'Product')
-		category = SubElement(categories, 'Category')
-		brand = SubElement(brands, 'Brand')
 
 		# Secondary product information (mfg. part no, UPC, model no., etc.)
+		if checkForExistence(line, 7, 'Brand information is missing'):
+			brand_dict[line[8]] = {
+				'ExternalId': line[8],
+				'Name': line[7]
+			}
+			populateTags(product, 'BrandExternalId', line[8])
+		if checkForExistence(line, 5, 'Category information is missing'):
+			category_dict[line[6]] = {
+				'ExternalId': line[6],
+				'Name': line[5]
+			}
 		if checkForExistence(line, 9, 'Secondary product information is missing (Mfg. Part Number)'):
 			mfgPartNumbers = SubElement(product, 'ManufacturerPartNumbers')
-			populateTags(mfgPartNumbers, 'ManufacturerPartNumber', line[9])
+			for mfgPartNumber in line[9].split(':'):
+				populateTags(mfgPartNumbers, 'ManufacturerPartNumber', mfgPartNumber)
 		if checkForExistence(line, 10, 'Secondary product information is missing (UPC)'):
 			upcs = SubElement(product, 'UPCs')
 			populateTags(upcs, 'UPC', line[10])
 		if checkForExistence(line, 11, 'Secondary product information is missing (Model Number)'):
 			modelNumbers = SubElement(product, 'ModelNumbers')
-			populateTags(modelNumbers, 'ModelNumber', line[11])
+			for modelNumber in line[11].split(':'):
+				populateTags(modelNumbers, 'ModelNumber', modelNumber)
 
 		# Product families
 		if checkForExistence(line, 12, 'Product families information is missing'):
-			if line[12] != '':
-				attrs = SubElement(product, 'Attributes')
-				familyAttr = SubElement(attrs, 'Attribute')
-				familyAttr.set('id', 'BV_FE_FAMILY')
-				familyValue = SubElement(familyAttr, 'Value')
-				familyValue.text = line[12]
-				# Expand product families
-				if checkForExistence(line, 13, ''):
-					if line[13] != '':
-						expandAttr = SubElement(attrs, 'Attribute')
-						expandAttr.set('id', 'BV_FE_EXPAND')
-						expandValue = SubElement(expandAttr, 'Value')
-						expandValue.text = 'BV_FE_FAMILY:' + line[12]
+			attrs = SubElement(product, 'Attributes')
+			familyAttr = SubElement(attrs, 'Attribute')
+			familyAttr.set('id', 'BV_FE_FAMILY')
+			familyValue = SubElement(familyAttr, 'Value')
+			familyValue.text = line[12]
+			# Expand product families
+			if checkForExistence(line, 13, ''):
+				if line[13] != '':
+					expandAttr = SubElement(attrs, 'Attribute')
+					expandAttr.set('id', 'BV_FE_EXPAND')
+					expandValue = SubElement(expandAttr, 'Value')
+					expandValue.text = 'BV_FE_FAMILY:' + line[12]
 
 		# Define lists
 		productList = {
@@ -96,29 +117,31 @@ def generateFeed(options):
 			'Description': productDesc,
 			'ProductPageUrl': productPageUrl,
 			'ImageUrl': productImageUrl,
-			'CategoryExternalId': categoryId,
-			'BrandExternalId': brandId
+			'CategoryExternalId': categoryId
 		}
 
-		categoryList = {
-			'Name': categoryName, 
-			'ExternalId': categoryId
-		}
-
-		brandList = {
-			'Name': brandName, 
-			'ExternalId': brandId
-		}
+		# categoryList = {
+		# 	'Name': categoryName, 
+		# 	'ExternalId': categoryId
+		# }
 
 		elementsMapToLists = {
 			product: productList,
-			category: categoryList,
-			brand: brandList
 		}
 
 		for key, value in elementsMapToLists.items():
 			for k, v in value.items():
 				populateTags(key, k, v)
+		
+	for key in brand_dict:
+		brand = SubElement(brands, 'Brand')
+		populateTags(brand, 'Name', brand_dict[key]['Name'])
+		populateTags(brand, 'ExternalId', brand_dict[key]['ExternalId'])
+
+	for key in category_dict:
+		category = SubElement(categories, 'Category')
+		populateTags(category, 'Name', category_dict[key]['Name'])
+		populateTags(category, 'ExternalId', category_dict[key]['ExternalId'])
 
 	clientProductFeed.write(xmlPrefix)
 	clientProductFeed.write(tostring(root))
@@ -140,7 +163,7 @@ def main(argv):
 	generateFeed(options)
 
 if __name__ == "__main__":
-    main(sys.argv[1:])	
+	main(sys.argv[1:])
 
 
 
