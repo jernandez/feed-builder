70ee14cb7c05d97e0e93dd8c01e73fa8be9dda44
javier.hernandez <javier.hernandez@bazaarvoice.com>
2013-09-13 13:54:32 -0500

    removing xml config, moved mapping out of file

---
+++ mapping.py
@@ -0,0 +1,22 @@
+def returnMap():
+
+	product_Map = {
+	'Name': 0,
+	'ExternalId': 1,
+	'ProductPageUrl': 2,
+	'Description': 3,
+	'ImageUrl': 4,
+	'CategoryExternalId': 5,
+	'CategoryPageUrl': 13,
+	'CategoryName': 6,
+	'CategoryImageUrl': 14,
+	'CategoryParentExternalId': 15,
+	'Brand': 7,
+	'BrandExternalId': 8,
+	'ManufacturerPartNumber': 9,
+	'UPC': 10,
+	'ModelNumber': 11,
+	'locale' : 12
+	}
+
+	return product_Map
